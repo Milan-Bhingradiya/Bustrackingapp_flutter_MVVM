1071a3890e26e8ea7cc0a4edf529017523e74d3e
Milan op <milanbhingradiya00@gmail.com>
2023-01-29 00:10:23 +0530

    chnage lots of code(main multiple bus track)

--- lib/screens/parentsscrens/parentwelcomescreen.dart
+++ lib/screens/parentsscrens/parentwelcomescreen.dart
@@ -1,12 +1,17 @@
 import 'dart:async';
+import 'dart:convert';
+
 //import 'dart:html';
 //import 'dart:html';
-import 'dart:io';
+import 'dart:io' as io;
+import 'dart:typed_data';
 import 'package:bustrackingapp/screens/parentsscrens/trackingbus/multiple_track/multiplemarker.dart';
 import 'package:bustrackingapp/screens/parentsscrens/drawer/parentdrawerwidget.dart';
 import 'package:bustrackingapp/screens/parentsscrens/trackingbus/select_single_or_multiple_track.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
@@ -21,25 +26,29 @@ class parentwelcomescreen extends StatefulWidget {
 }
 
 class _parentwelcomescreenState extends State<parentwelcomescreen> {
+  io.File? final_img;
 
-  File? final_img;
-  late final imaegtemporary;
-
-//   Future uploadfile() async {
-//  // final file = File( image.path);
-//   }
-
-  select_img() async {
-    
+  final storageRef = FirebaseStorage.instance.ref();
+  Future<void> select_img() async {
     final image = await ImagePicker().pickImage(source: ImageSource.gallery);
 
-    if (image != null) {
-   final imaegtemporary = File(image.path);
-    }
-
     setState(() {
-      this.final_img = imaegtemporary;
+      if (image != null) {
+        final_img = io.File(image.path);
+      }
     });
+  }
+
+  Future<void> uploadPic() async {
+    String url = "";
+    try {
+      //  url = await mountainImagesRef.getDownloadURL();
+      final ref = storageRef.child("images/first");
+      ref.putFile(final_img!);
+    
+    } catch (e) {
+      print(e);
+    }
   }
 
   @override
@@ -62,32 +71,36 @@ class _parentwelcomescreenState extends State<parentwelcomescreen> {
           children: [
             // for checking i am able to make stream or not just for example
             GestureDetector(
+                onTap: () async {
+                  select_img();
+                },
+                child: CircleAvatar(
+                    radius: 100,
+                    backgroundImage:
+                        final_img == null ? null : FileImage(final_img!),
+                    child: final_img == null
+                        ? Container(
+                            child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.person),
+                              Text("select profile")
+                            ],
+                          ))
+                        : null)),
+
+            GestureDetector(
               onTap: () async {
-                print("touch");
-                // List<Placemark> placemarks = await placemarkFromCoordinates(
-                //     21.22461083368087, 72.77901540143989);
-                // print(placemarks.last.street);
-
-                select_img();
-
-                // Navigator.push(
-                //     context,
-                //     MaterialPageRoute(
-                //         builder: ((context) => select_single_or_multiple_track())));
-
-                // print(DocumentSnapshot.);
+                await uploadPic().then((value) {
+                  Fluttertoast.showToast(msg: "your profile has been updated");
+                });
+                //  print(url_of_img);
               },
               child: Container(
-                  height: 200,
-                  width: 200,
-                  color: Colors.amber,
-                  child: (final_img != null)
-                      ? Image.file(
-                          final_img!,
-                          height: 200,
-                          width: 200,
-                        )
-                      : Container()),
+                height: 100,
+                width: 100,
+                color: Colors.amber,
+              ),
             )
           ],
         ),
